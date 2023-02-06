ce8f2baec2c325adbcfc94df2f320197639a2919
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2023-02-06 17:59:12 -0300

    Create Dynamic Pages - 04/05

--- JavaScript for the Web - Create Dynamic Pages/main.js
+++ JavaScript for the Web - Create Dynamic Pages/main.js
@@ -1,7 +1,15 @@
-function playSoundPom() {
-	document.querySelector("#sound_key_pom").play();
-}
-
 const listOfKeys = document.querySelectorAll(".key");
 
-listOfKeys[0].onclick = playSoundPom;
+for (let count = 0; count < listOfKeys.length; count++) {
+	const key = listOfKeys[count];
+	const instrument = key.classList[1];
+	const idAudio = `#sound_${instrument}`;
+
+	key.onclick = function () {
+		playSound(idAudio);
+	};
+}
+
+function playSound(idSoundElement) {
+	document.querySelector(idSoundElement).play();
+}
