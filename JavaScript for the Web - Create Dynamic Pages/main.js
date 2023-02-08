425b1aecc190994ee219c4b016d44b396dabf896
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2023-02-08 09:10:32 -0300

    Create Dynamic Pages - 05/05 (refactored)

--- JavaScript for the Web - Create Dynamic Pages/main.js
+++ JavaScript for the Web - Create Dynamic Pages/main.js
@@ -5,16 +5,26 @@ for (let count = 0; count < listOfKeys.length; count++) {
 	const instrument = key.classList[1];
 	const idAudio = `#sound_${instrument}`;
 
+	playSoundOnClick(key, idAudio);
+	addActiveClassOnKeyPress(key);
+	removeActiveClassOnKeyRelease(key);
+}
+
+function playSoundOnClick(key, idAudio) {
 	key.onclick = function () {
 		playSound(idAudio);
 	};
+}
 
+function addActiveClassOnKeyPress(key) {
 	key.onkeydown = function (event) {
 		if (event.code === "Enter" || event.code === "Space") {
 			key.classList.add("active");
 		}
 	};
+}
 
+function removeActiveClassOnKeyRelease(key) {
 	key.onkeyup = function () {
 		key.classList.remove("active");
 	};
