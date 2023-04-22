8403618e9fdf9eb60526baf24a0a78de24c38787
wagnermarques00 <wagnermarques00@gmail.com>
2023-04-22 19:55:51 -0300

    Consuming an API - 03/05

--- JavaScript - Consuming and handling data from an API/src/js/script.js
+++ JavaScript - Consuming and handling data from an API/src/js/script.js
@@ -1,3 +1,11 @@
-let searchCEP = fetch("https://viacep.com.br/ws/01001000/json/");
-
-console.log(searchCEP);
+let searchCEP = fetch("https://viacep.com.br/ws/01001000/json/")
+	.then((response) => response.json())
+	.then((response) => {
+		if (response.erro) {
+			throw Error("CEP not found");
+		} else {
+			console.log(response);
+		}
+	})
+	.catch((error) => console.warn(error))
+	.finally((message) => console.log("Processing done!"));
