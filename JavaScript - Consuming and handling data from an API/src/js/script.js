bfd3deae3854bbaa73f7c584917b618c071f8de4
wagnermarques00 <wagnermarques00@gmail.com>
2023-04-23 17:15:08 -0300

    Consuming an API - 05/05

--- JavaScript - Consuming and handling data from an API/src/js/script.js
+++ JavaScript - Consuming and handling data from an API/src/js/script.js
@@ -11,6 +11,9 @@
 // 	.finally(console.log("Processing done!"));
 
 async function searchAdress(cep) {
+	let errorMessage = document.getElementById("cep-error");
+	errorMessage.innerHTML = "";
+
 	try {
 		let searchCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
 		let convertedSearchCEP = await searchCEP.json();
@@ -18,15 +21,31 @@ async function searchAdress(cep) {
 		if (convertedSearchCEP.erro) {
 			throw Error("CEP not found");
 		}
+
+		let street = document.getElementById("address");
+		let complement = document.getElementById("complement");
+		let neighborhood = document.getElementById("neighborhood");
+		let city = document.getElementById("city");
+		let state = document.getElementById("state");
+
+		street.value = convertedSearchCEP.logradouro;
+		complement.value = convertedSearchCEP.complemento;
+		neighborhood.value = convertedSearchCEP.bairro;
+		city.value = convertedSearchCEP.localidade;
+		state.value = convertedSearchCEP.uf;
+
 		console.log(convertedSearchCEP);
 		return convertedSearchCEP;
 	} catch (error) {
+		errorMessage.innerHTML = `<p>CEP inválido. Tente novamente!</p>`;
 		console.error(error);
 	} finally {
 		console.log("Processing done!");
 	}
 }
 
+let cep = document.getElementById("cep");
+cep.addEventListener("focusout", () => searchAdress(cep.value));
 // Promise.all is very useful in situations where we need to perform multiple asynchronous operations and wait for all of them to complete before continuing with processing.
 // let ceps = ["01001000", "01001001"];
 // let cepsSet = ceps.map((value) => searchAdress(value));
